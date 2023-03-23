16b3e6756eab2498fdaeab08724e65b6028a1243
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-03-23 13:44:53 -0300

    Add date functions

--- navigation.js
+++ navigation.js
@@ -27,3 +27,12 @@ contactInfo.addEventListener('click', () => {
   bookLists.style.display = 'none';
   contact.style.display = 'flex';
 });
+
+function displayDate() {
+  const currentDate = new Date();
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  const formattedDate = currentDate.toLocaleDateString('en-US', options);
+  document.getElementById("date").innerHTML = formattedDate;
+}
+
+window.addEventListener("load", displayDate);
