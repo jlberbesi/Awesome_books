3c1ff5f1fd6194f329cc5fe659ac09b0cc4df96d
Jlberbesi <jlberbesi.walloom@gmail.com>
2023-07-08 17:21:25 -0500

    JS correction

--- script.js
+++ script.js
@@ -57,5 +57,3 @@ function addBookToCollection(event) {
 // Display the initial book list
 displayBooks();
 
-// Add an event listener to the form submission
-addBookForm.addEventListener('submit', addBookToCollection);
